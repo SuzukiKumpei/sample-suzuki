ba0fe996eaaa95ac23053c68ca2d06e6f71757c7
Hiroki321996 <hiroki.tani@rakus-partners.co.jp>
2020-02-27 15:28:34 +0900

    gradeをフィールド変数として追加しました

--- src/main/java/com/example/Classroom.java
+++ src/main/java/com/example/Classroom.java
@@ -3,6 +3,23 @@ package com.example;
 public class Classroom {
 	
 	private String name;
+	private String grade;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
 
 	@Override
 	public String toString() {
