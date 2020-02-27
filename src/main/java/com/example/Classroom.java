50a6b382f0a07d3d72602ff26df430dc038120a7
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-27 15:34:03 +0900

    新たなフィールド変数を作成しました。

--- src/main/java/com/example/Classroom.java
+++ src/main/java/com/example/Classroom.java
@@ -3,10 +3,36 @@ package com.example;
 public class Classroom {
 	
 	private String name;
+	private String homeRoomTeacherName;
 
 	@Override
 	public String toString() {
-		return "Classroom [name=" + name + "]";
+		return "Classroom [name=" + name + ", homeRoomTeacherName=" + homeRoomTeacherName + "]";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Classroom() {
+	}
+
+	public Classroom(String name, String homeRoomTeacherName) {
+		super();
+		this.name = name;
+		this.homeRoomTeacherName = homeRoomTeacherName;
+	}
+
+	public String getHomeRoomTeacherName() {
+		return homeRoomTeacherName;
+	}
+
+	public void setHomeRoomTeacherName(String homeRoomTeacherName) {
+		this.homeRoomTeacherName = homeRoomTeacherName;
 	}
 	
 	
